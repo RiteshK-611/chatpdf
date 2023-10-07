370759a0a2a9edbc777b25a2f0ec38b366b489e4
Ritesh <riteshkokam@gmail.com>
2023-10-07 23:56:38 +0530

    add packages and update pinecone.ts file

---
+++ src/lib/embedding.ts
@@ -0,0 +1,21 @@
+import { Configuration, OpenAIApi } from "openai-edge"
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+})
+
+const openai = new OpenAIApi(configuration)
+
+export const getEmbeddings = async (text: string) => {
+    try {
+        const response = await openai.createEmbedding({
+            model: "text-embedding-ada-002",
+            input: text.replace(/\n/g, "")
+        })
+        const result = await response.json();
+        return result.data[0].embedding as number[]
+    } catch (error) {
+        console.log("error calling openai embedding api", error)
+        throw error
+    }
+}
