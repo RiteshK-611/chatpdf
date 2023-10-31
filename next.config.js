70c7c9abaf48750c01cff58480fa1fe2c14c427b
Ritesh <riteshkokam@gmail.com>
2023-10-31 23:56:53 +0530

    update config file & add hugginface embeding model

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }) => {
+    config.module.rules.push({
+      test: /\.node$/i,
+      use: "node-loader",
+    });
+    // Important: return the modified config
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/lib/embedding.ts
+++ src/lib/embedding.ts
@@ -1,23 +1,42 @@
 import { Configuration, OpenAIApi } from "openai-edge";
+import { HuggingFaceTransformersEmbeddings } from "langchain/embeddings/hf_transformers";
 
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+// HuggingFace
+const model = new HuggingFaceTransformersEmbeddings({
+  modelName: "Xenova/all-MiniLM-L6-v2",
 });
-
-const openai = new OpenAIApi(configuration);
 
 export const getEmbeddings = async (text: string) => {
   try {
-    console.log("\nOPENAI: ", process.env.OPENAI_API_KEY);
-    const response = await openai.createEmbedding({
-      model: "text-embedding-ada-002",
-      input: text.replace(/\n/g, " "),
-    });
-    const result = await response.json();
-    console.log("\nResult: ", result);
-    return result.data[0].embedding as number[];
+    console.log("HuggingFaceTransformersEmbeddings");
+    const response = await model.embedQuery(text.replace(/\n/g, " "));
+    
+    return response;
   } catch (error) {
-    console.log("error calling openai embedding api", error);
+    console.log("error calling hugginface embedding api", error);
     throw error;
   }
 };
+
+// OpenAI
+// const configuration = new Configuration({
+//   apiKey: process.env.OPENAI_API_KEY,
+// });
+
+// const openai = new OpenAIApi(configuration);
+
+// export const getEmbeddings = async (text: string) => {
+//   try {
+//     console.log("\nOPENAI: ", process.env.OPENAI_API_KEY);
+//     const response = await openai.createEmbedding({
+//       model: "text-embedding-ada-002",
+//       input: text.replace(/\n/g, " "),
+//     });
+//     const result = await response.json();
+//     console.log("\nResult: ", result);
+//     return result.data[0].embedding as number[];
+//   } catch (error) {
+//     console.log("error calling openai embedding api", error);
+//     throw error;
+//   }
+// };
