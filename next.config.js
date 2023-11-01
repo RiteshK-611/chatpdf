4703ea4e90e0c8c602933b6b8af246394b6e8c71
Ritesh <riteshkokam@gmail.com>
2023-11-01 23:52:45 +0530

    remove huggingface embedder

--- next.config.js
+++ next.config.js
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }) => {
-    config.module.rules.push({
-      test: /\.node$/i,
-      use: "node-loader",
-    });
-    // Important: return the modified config
-    return config;
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
